e71d635e6f015496ee59b0e56694f495b4aeda63
Snowirbix <snowirbix@gmail.com>
2019-07-22 21:02:43 +0200

    Add Camera Follow

---
+++ Assets/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class CameraController : NetworkBehaviour
+{
+    private void Start ()
+    {
+        if (isLocalPlayer)
+        {
+            GameObject cam = GameObject.Find("vCam");
+            if (cam)
+            {
+                Cinemachine.CinemachineVirtualCamera vCam = cam.GetComponent<Cinemachine.CinemachineVirtualCamera>();
+                vCam.Follow = transform;
+                vCam.LookAt = transform;
+            }
+        }
+    }
+}
